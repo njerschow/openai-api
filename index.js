77b54d1e4e77d307218df8746c2e23b2772a18ce
njerschow <nj18@nyu.edu>
2020-07-25 06:25:13 -0700

    test fixes

---
+++ index.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const config = require('./config'),
+    axios = require('axios');
+
+class OpenAI {
+    constructor(api_key) {
+        this.__api_key = api_key;
+    }
+
+    __send_request = (opts) => {
+        const url = config.completionURL(opts.engine);
+        const reqOpts = {
+            headers: {
+                'Authorization': `Bearer ${this.__api_key}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const data = {
+            prompt: opts.prompt,
+            max_tokens: opts.maxTokens,
+            temperature: opts.temperature,
+            top_p: opts.topP,
+            n: opts.n,
+            stream: opts.stream,
+            logprobs: opts.logprobs,
+            stop: opts.stop
+        };
+        return axios.post(url, data, reqOpts);
+    }
+
+    create = function (opts) {
+        return this.__send_request(opts);
+    }
+
+    search = (opts) => {
+        const url = config.searchURL(opts.engine);
+        const reqOpts = {
+            headers: {
+                'Authorization': `Bearer ${this.__api_key}`,
+                'Content-Type': 'application/json'
+            }
+        };
+        const data = {
+            documents: opts.documents,
+            query: opts.query
+        };
+        return axios.post(url, data, reqOpts);
+    }
+}
+
+module.exports = OpenAI;
---
+++ test/basic.js
@@ -0,0 +1,41 @@
+const api_key = process.env.OPENAI_TEST_API_KEY;
+const assert = require('assert');
+
+const OpenAI = require('../index');
+
+if (!api_key) {
+    throw 'api key is needed to run testsuite: set environment variable: OPENAI_TEST_API_KEY'
+}
+
+describe('basic openai api methods', function () {
+
+    const openai = new OpenAI(api_key);
+
+    it ('handle simple completion', function (done) {
+        openai.create({
+            prompt: "this is a test",
+            maxTokens: 5
+        }).then((result) => {
+            assert.ok(result);
+            done();
+        })
+        .catch(err => {
+            console.error(err);
+            assert.fail();
+        });
+    });
+
+    it ('handle search', function (done) {
+        openai.search({
+            documents: ["White House", "hospital", "school"],
+            query: "the president"
+        }).then((result) => {
+            assert.ok(result);
+            done();
+        })
+        .catch(err => {
+            console.error(err);
+            assert.fail();
+        })
+    })
+});
