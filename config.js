d21936c0e2c07e64cdeaf0326ae027d99bc1f0a2
njerschow <nj18@nyu.edu>
2020-08-09 15:11:45 -0700

    remove engine check

--- config.js
+++ config.js
@@ -6,17 +6,11 @@ module.exports = {
         if (!engine) {
             engine = DEFAULT_ENGINE;
         }
-        if (!ENGINE_LIST.includes(engine)) {
-            throw new Error("< " + engine + " > is not a valid OpenAI engine.");
-        }
         return `https://api.openai.com/v1/engines/${engine}/completions`;
     },
     searchURL: (engine) => {
         if (!engine) {
             engine = DEFAULT_ENGINE;
-        }
-        if (!ENGINE_LIST.includes(engine)) {
-            throw new Error("< " + engine + " > is not a valid OpenAI engine.");
         }
         return `https://api.openai.com/v1/engines/${engine}/search`;
     }
