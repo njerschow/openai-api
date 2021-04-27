1937cc706df5a2d20379312d0032fd9aec4bcfbd
Corman <32941017+Cormanz@users.noreply.github.com>
2021-04-27 12:14:50 -0400

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,63 @@
+declare module 'openai-api' {
+    interface CompletionOpts {
+        engine: string;
+        prompt?: string;
+        maxTokens?: number;
+        tempature?: number;
+        topP?: number;
+        n?: number;
+        stream?: boolean;
+        logprobs?: number;
+        echo?: boolean;
+        stop?: string | string[];
+        presencePenalty?: number;
+        frequencyPenalty?: number;
+        bestOf?: number;
+        logitBias?: { [ token: string ]: number }
+    }
+
+    export interface Completion {
+        id: string
+        object: string
+        created: number
+        model: string
+        choices: Choice[]
+      }
+      
+    export interface Choice {
+        text: string
+        index: number
+        logprobs: any
+        finish_reason: string
+    }
+     
+    export interface SearchOpts {
+        engine: string;
+        documents?: string[];
+        file?: string;
+        query: string
+        maxRerank?: number;
+        returnMetadata?: boolean;
+      }      
+
+    export interface Search {
+        data: Document[];
+        object: string;
+    }
+    
+    export interface Document {
+        document: number;
+        object: string;
+        score: number;
+        metadata?: any;
+    }
+      
+
+    class OpenAI {
+        constructor(api: string);
+        complete(opts: CompletionOpts): Promise<Completion>;
+        encode(str: string): number[];
+        search(opts: SearchOpts): Promise<Search>;
+    }
+    export default OpenAI;
+}
