ff8efba93043701377f837e901950c3624d21e8c
njerschow <nj18@nyu.edu>
2021-08-17 18:51:47 -0400

    remove engines list

--- config.js
+++ config.js
@@ -1,20 +1,13 @@
 const DEFAULT_ENGINE = 'davinci';
-const ENGINE_LIST = ['ada', 'babbage', 'curie', 'davinci', 'davinci-codex', 'davinci-instruct-beta', 'curie-instruct-beta', 'content-filter-alpha-c4'];
 const ORIGIN = 'https://api.openai.com';
 const API_VERSION = 'v1';
 const OPEN_AI_URL = `${ORIGIN}/${API_VERSION}`
 
 module.exports = {
   completionURL(engine) {
-    if (!ENGINE_LIST.includes(engine)) {
-      engine = DEFAULT_ENGINE;
-    }
     return `${OPEN_AI_URL}/engines/${engine}/completions`;
   },
   searchURL(engine) {
-    if (!ENGINE_LIST.includes(engine)) {
-      engine = DEFAULT_ENGINE;
-    }
     return `${OPEN_AI_URL}/engines/${engine}/search`;
   },
   enginesUrl() {
