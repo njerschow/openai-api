3dc9edd3c5eacd378799e5480a8e2dcd506364ea
Stefan Neidig <stefan@rpdev.net>
2022-02-18 09:24:15 +0100

    Add typings for embeddings endpoint

--- index.d.ts
+++ index.d.ts
@@ -14,47 +14,58 @@ declare module 'openai-api' {
         frequencyPenalty?: number;
         bestOf?: number;
         user?: string;
-        logitBias?: { [ token: string ]: number }
+        logitBias?: { [token: string]: number; };
     }
 
     export interface Completion {
         data: {
-            id: string
-            object: string
-            created: number
-            model: string
-            choices: Choice[]
-        }
-      }
-      
-    export interface Choice {
-        text: string
-        index: number
-        logprobs: any
-        finish_reason: string
+            id: string;
+            object: string;
+            created: number;
+            model: string;
+            choices: Choice[];
+        };
     }
-     
+
+    export interface Choice {
+        text: string;
+        index: number;
+        logprobs: any;
+        finish_reason: string;
+    }
+
     export interface SearchOpts {
         engine: string;
         documents?: string[];
         file?: string;
-        query: string
+        query: string;
         maxRerank?: number;
         returnMetadata?: boolean;
-      }      
+    }
 
     export interface Search {
         data: Document[];
         object: string;
     }
-    
+
     export interface Document {
         document: number;
         object: string;
         score: number;
         metadata?: any;
     }
-      
+
+    export interface Embedding {
+        object: string;
+        data: EmbeddingData[];
+        model: string;
+    }
+
+    export interface EmbeddingData {
+        object: string;
+        index: number;
+        embedding: number[];
+    }
 
     class OpenAI {
         constructor(api: string);
@@ -62,6 +73,6 @@ declare module 'openai-api' {
         encode(str: string): number[];
         search(opts: SearchOpts): Promise<Search>;
     }
-    
+
     export default OpenAI;
 }
