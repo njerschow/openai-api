3cbc27cef92ae40754dd2a99c902677e81e18fe1
Stefan Neidig <stefan@rpdev.net>
2022-06-16 15:20:32 +0200

    Convert package to typescript package

---
+++ src/config.ts
@@ -0,0 +1,35 @@
+const ORIGIN = "https://api.openai.com";
+const API_VERSION = "v1";
+const OPEN_AI_URL = `${ORIGIN}/${API_VERSION}`;
+
+export function completionURL(engine: string): string {
+  return `${OPEN_AI_URL}/engines/${engine}/completions`;
+}
+
+export function searchURL(engine: string): string {
+  return `${OPEN_AI_URL}/engines/${engine}/search`;
+}
+
+export function enginesUrl(): string {
+  return `${OPEN_AI_URL}/engines`;
+}
+
+export function engineUrl(engine: string): string {
+  return `${OPEN_AI_URL}/engines/${engine}`;
+}
+
+export function classificationsUrl(): string {
+  return `${OPEN_AI_URL}/classifications`;
+}
+
+export function filesUrl(): string {
+  return `${OPEN_AI_URL}/files`;
+}
+
+export function answersUrl(): string {
+  return `${OPEN_AI_URL}/answers`;
+}
+
+export function embeddingsUrl(engine: string): string {
+  return `${OPEN_AI_URL}/engines/${engine}/embeddings`;
+}
